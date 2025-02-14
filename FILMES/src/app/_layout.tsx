24c4339f904eb2cb06fade45ba4fbc611c42c151
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-02-14 12:37:52 -0300

    :fire: refactor: remover arquivos desnecessários e simplificar layout

--- FILMES/src/app/_layout.tsx
+++ FILMES/src/app/_layout.tsx
@@ -1,13 +1,5 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack 
-      screenOptions={{
-        headerShown: false,
-    }}
-  >
-      <Stack.Screen name="index" />
-    </Stack>
-    );
+  return <Stack />;
 }
