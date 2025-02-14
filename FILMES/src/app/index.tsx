faddea26cd9f7bf7ce8644b50d9f59dede7fead8
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-02-14 12:41:21 -0300

    :sparkles: feat: adicionar campo de busca e estilização na tela inicial

--- FILMES/src/app/index.tsx
+++ FILMES/src/app/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TextInput } from "react-native";
+import Foundation from "@expo/vector-icons/Foundation";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>O que você quer assistir?</Text>
+
+      <View style={styles.headerInput}>
+        <TextInput
+          style={styles.input}
+          placeholder="Buscar" 
+          placeholderTextColor="#FFF"
+        />
+
+        <Foundation name="magnifying-glass" size={24} color="#FFF" />
+      </View>
     </View>
   );
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#242A32",
+    paddingHorizontal: 30,
+    paddingTop: 35,
+  },
+  title: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 24,
+    textAlign: "center",
+  },
+  headerInput: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#3A3F47",
+    padding: 10,
+    borderRadius: 16,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+  },
+
+});
